c5fefe6e36c2009f93e3279544ae85889b5f4178
Naurangi Lal <107787672+Naurangi123@users.noreply.github.com>
2022-11-15 17:02:20 -0800

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -1,93 +1,64 @@
-"""Square Pattern
-*****
-*****
-*****
-*****
-*****
-"""
+#                                 ********************************************************
+# for i in range(6):
+#     for j in range(i):
+#         print(end="*")
+#     print()
+# k=0
+# for i in range(6):
+#     for j in range(6-i):
+#         k+=1
+#         print("*",end="")
+#     print()
+#                          ***********************************************************
+
+#      ***** 
+#     ***** 
+#    *****
+#   *****
+#  *****
+
+# for i in range(0,5):
+#     for j in range(5-i):
+#         print(" ",end="")
+#     for k in range(5):
+#         print("*",end="")
+#     print(" ")
+#                              *********************************************************
+# * * * * * 
+# * * * * 
+# * * *
+# * *
+# *
+
+# for i in range(5):
+#     for j in range(5-i):
+#         print(" *",end=" ")
+#     print("")
+ #                              **************************************** 
+
+# for i in range(10):
+#     for j in range(i):
+#         print(j+1,end=" ")
+#     print()
 
 
-for i in range(6):
-    for j in range(5):
-        print("*",end="")
-    print()
-    
+#                            ****************************************************
+
+# for i in range(10):
+#     print(' ' * (10 - i) + '*' * (2 * i))
+
+#                             *****************************************************
+# for i in range(10):
+#     for j in range(10-i):
+#         print(" ",end=" ")
+#     print(" *" * (2 * i))
 
 
-print("=="*10)
+#                                ***********************************************************
 
-"""
-*
-**
-***
-****
-*****
-******
-"""
-
-
-for i in range(6):
-    for j in range(i+1):
-        print("*",end="")
-    print()
-
-print("=="*10)
-"""
-*****
-****
-***
-**
-*
-"""
-for i in range(6):
-    for j in range(6,i,-1):
-        print("*",end="")
-    print()
-
-print("=="*10)
-"""
-*
-**
-***
-****
-*****
-******
-*****
-****
-***
-**
-*
-"""
-for i in range(6):
-    for j in range(i+1):
-        print("",end="*")
-    print()
-print("*"*7)
-for i in range(6):
-    for j in range(6,i,-1):
-        print("*",end="")
-    print()
-
-print("=="*10)
-"""
-    *
-   **
-  ***
- ****
-*****
-"""
-for i in range(6):
-    for j in range(i):
-        print(end="*")
-    print()
-
-print("=="*10)
-
-k=0
-for i in range(6):
-    for j in range():
-        k+=1
-        print(j,end="")
-    print()
-
-print("=="*10)
+# k= 1
+# for i in range(1,5):
+#     for j in range(i):
+#         print(k, end=' ')
+#         k = k + 1
+#     print()
